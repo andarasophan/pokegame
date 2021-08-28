8882123468eb50b83a6d2a664adc59ba7be71def
andarasophan <andarasophan@gmail.com>
2021-08-28 22:05:22 +0700

    feat: add component LoadingSpinner

--- src/assets/icons.js
+++ src/assets/icons.js
@@ -1,1 +1,13 @@
+import React from 'react'
+
 // for icons
+export const Spinner = ({ size = 24, className }) => (
+  <svg
+    className={className}
+    viewBox="22 22 44 44"
+    width={size}
+    height={size}
+  >
+    <circle cx="44" cy="44" r="20.2" fill="none" strokeWidth="3.6" stroke="currentColor" />
+  </svg>
+)
---
+++ src/components/LoadingSpinner.js
@@ -0,0 +1,51 @@
+/** @jsxImportSource @emotion/react */
+import { css, keyframes, useTheme } from '@emotion/react'
+import { Spinner } from '../assets/icons'
+
+const spinnerRotate = keyframes`
+  100% {
+    transform: rotate(360deg);
+  }
+`
+const spinnerDash = keyframes`
+  0% {
+    stroke-dasharray: 1px, 200px;
+    stroke-dashoffset: 0px;
+  }
+  50% {
+    stroke-dasharray: 100px, 200px;
+    stroke-dashoffset: -15px;
+  }
+  100% {
+    stroke-dasharray: 100px, 200px;
+    stroke-dashoffset: -125px;
+  }
+`
+
+const LoadingSpinner = ({ className, size, color = 'primary' }) => {
+  const theme = useTheme()
+
+  return (
+    <div
+      css={{ display: 'inline-flex' }}
+      className={className}
+    >
+      <Spinner
+        size={size}
+        css={css`
+          display: block;
+          animation: ${spinnerRotate} 1.4s linear infinite;
+          color: ${theme.colors[color]};
+
+          circle {
+            animation: ${spinnerDash} 1.4s ease-in-out infinite;
+            stroke-dasharray: 80px, 200px;
+            stroke-dashoffset: 0px;
+          }
+        `}
+      />
+    </div>
+  )
+}
+
+export default LoadingSpinner

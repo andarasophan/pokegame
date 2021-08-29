04510c0834806e4fe31f0b0ac55a1e7947854e3b
andarasophan <andarasophan@gmail.com>
2021-08-30 01:14:50 +0700

    layout: handle rendering header

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -1,12 +1,24 @@
-import React from 'react'
+import React, { useMemo } from 'react'
+import { useLocation } from 'react-router'
 import Header from './Header'
+
+const routeWithHeader = [
+  '/',
+  '/detail'
+]
 
 const Layout = ({
   children
 }) => {
+  const { pathname } = useLocation()
+  const firstPathname = useMemo(() => `/${pathname.split('/')[1]}`, [pathname])
+
   return (
     <main>
-      <Header />
+      {
+        routeWithHeader.includes(firstPathname) &&
+        <Header />
+      }
       {children}
     </main>
   )
